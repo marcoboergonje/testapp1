f6bb66cf2807611bfec9ed18c6c87c5218c7f318
nickglas <40456410+nickglas@users.noreply.github.com>
2018-12-03 23:14:27 +0100

    Naam , Pass , Button
    UI Restyle(Lang niet af)....
    
    Naam en pass worden getoond als ze zijn ingevuld via toast melding.
    
    button met onclick toegevoegd.
    (Try catch block)
    
    App heeft geen apptheme rand meer met app naam.

--- app/src/main/java/com/example/boergonje/testapp1/MainActivity.java
+++ app/src/main/java/com/example/boergonje/testapp1/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.boergonje.testapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void Login(View view){
+        try{
+            EditText Email = (EditText)findViewById(R.id.EditTextEmail);
+            EditText Wachtwoord = (EditText)findViewById(R.id.EditTextWachtwoord);
+
+            Toast.makeText(MainActivity.this, "Naam : " + Email.getText().toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Pass : " + Wachtwoord.getText().toString(), Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(MainActivity.this, "ERROR : " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+
     }
 }
